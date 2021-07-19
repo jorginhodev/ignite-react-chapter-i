9a8e8e57240661dad966cedc08635c9d6ecd6436
Jorge Ramos <jorginhodeveloper@gmail.com>
2021-07-19 11:46:38 -0300

    feat: list api repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,12 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -18,17 +12,14 @@ export function RepositoryList() {
       .then(data => setRepositories(data));
   }, []);
 
-  console.log(repositories);
-
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        { repositories.map(repository => 
+          <RepositoryItem key={repository.id} repository={repository} />
+        ) }
       </ul>
     </section>
   );
